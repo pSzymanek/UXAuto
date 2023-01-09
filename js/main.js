d0d0db44251d70fa82ea94cc3f2b555e04bdfc16
pSzymanek <poczta.piotrszymanek@gmail.com>
2023-01-09 23:14:03 +0100

    Finished JS, site ready to go.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector(".nav");
-const navBtn = document.querySelector(".burger-btn");
+const navBtn = document.querySelector(".burger__btn");
 const allNavItems = document.querySelectorAll(".nav__item")
+const navBtnBars = document.querySelector('.burger__btn-bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle("nav--active")
@@ -24,4 +27,28 @@ const handleNavItemsAnimation = () => {
     })
 }
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+
+        
+    })
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+
+
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
+window.addEventListener('scroll', handleObserver);
